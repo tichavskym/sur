aa98c53296e2fb211703eef23ac4cf4741cf6152
Milan Tichavský <milantichavsky@seznam.cz>
2024-04-19 11:50:37 +0200

    Add simple evaluation of GMM

--- audio/gmm.py
+++ audio/gmm.py
@@ -1,10 +1,10 @@
 import librosa
 import numpy as np
-import os.path
 
 from glob import glob
-from ikrlib import train_gmm
+from ikrlib import train_gmm, logpdf_gmm
 from librosa.feature import mfcc
+from pathlib import Path
 from numpy.random import randint
 
 SAMPLE_RATE = 16000
@@ -12,7 +12,9 @@ GMM_COMPONENTS = 16
 ITERATIONS = 10
 
 
-def load_recordings(dir_name: str, n_mfcc=13, initial_pause=1.7) -> list[tuple[str, np.ndarray]]:
+def load_recordings(
+    dir_name: str, n_mfcc=13, initial_pause=1.7
+) -> list[tuple[str, np.ndarray]]:
     """
     Load all *.wav files sampled at `SAMPLE_RATE` from directory `dir_name`, convert them into MFCC features and
     return them as array of tuples [(segment_name, mfcc)].
@@ -20,17 +22,40 @@ def load_recordings(dir_name: str, n_mfcc=13, initial_pause=1.7) -> list[tuple[s
     First `INITIAL_PAUSE` seconds of each recording are removed.
     """
     mfccs = []
-    for f in glob(dir_name + '/*.wav'):
-        segment_name = os.path.basename(f)
+    for f in glob(dir_name + "/*.wav"):
+        segment_name = Path(f).stem
         waveform, sr = librosa.load(f, sr=None)
         assert sr == SAMPLE_RATE
-        waveform = waveform[int(initial_pause * sr):]
+        waveform = waveform[int(initial_pause * sr) :]
         mfccs.append((segment_name, mfcc(y=waveform, sr=sr, n_mfcc=n_mfcc)))
 
     return mfccs
 
 
-if __name__ == '__main__':
+def evaluate(
+    recordings: list[(str, np.ndarray)],
+    weights: np.ndarray,
+    means: np.ndarray,
+    covs: np.ndarray,
+    nweights: np.ndarray,
+    nmeans: np.ndarray,
+    ncovs: np.ndarray,
+    p_t=0.5,
+    p_nt=0.5,
+):
+    for segment_name, features in recordings:
+        posterior_t = sum(logpdf_gmm(features.T, weights, means, covs)) + np.log(p_t)
+        posterior_nt = sum(logpdf_gmm(features.T, nweights, nmeans, ncovs)) + np.log(
+            p_nt
+        )
+
+        score = posterior_t - posterior_nt
+        decision = 1 if (score > 0) else 0
+        print(f"{segment_name} {score} {decision}")
+
+
+if __name__ == "__main__":
+    # Training
     targets = load_recordings("data/target_train")
     non_targets = load_recordings("data/non_target_train")
 
@@ -53,4 +78,13 @@ if __name__ == '__main__':
     for i in range(ITERATIONS):
         weights, means, covs, tll = train_gmm(targets, weights, means, covs)
         nweights, nmeans, ncovs, tll2 = train_gmm(non_targets, nweights, nmeans, ncovs)
-        print(f"Total log likelihood for target class: {tll}; for non-target class: {tll2}")
+        print(
+            f"Total log likelihood for target class: {tll}; for non-target class: {tll2}"
+        )
+
+    # Evaluation
+    recordings = load_recordings("data/target_dev")
+    evaluate(recordings, weights, means, covs, nweights, nmeans, ncovs)
+    print()
+    recordings = load_recordings("data/non_target_dev")
+    evaluate(recordings, weights, means, covs, nweights, nmeans, ncovs)
