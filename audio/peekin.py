e809c9c4920f8db46c5717f94d503525930bf9ab
Milan Tichavský <milantichavsky@seznam.cz>
2024-04-21 14:03:38 +0200

    Enable evaluation with augmented data

--- audio/peekin.py
+++ audio/peekin.py
@@ -4,21 +4,26 @@ from glob import glob
 from gmm import classify
 from contextlib import redirect_stdout
 
-for file in glob("models/" + "gmm_24*.npz"):
+FILENAME = "gmm_24*.npz"
+AUGMENTED = True
+TARGETS = 5*10 if AUGMENTED else 10
+TOTAL = 5*70 if AUGMENTED else 70
+
+for file in glob("models/" + FILENAME):
     f = io.StringIO()
     with redirect_stdout(f):
-        classify(f"{file}", "data/target_dev")
-        classify(f"{file}", "data/non_target_dev")
+        classify(f"{file}", "data/target_dev", augmentation=AUGMENTED)
+        classify(f"{file}", "data/non_target_dev", augmentation=AUGMENTED)
     results = f.getvalue()
 
     i = 0
     loss = 0
     for line in results.split("\n"):
-        if i == 70:
+        if i == TOTAL:
             continue
 
         score = line.split(" ")[1]
-        if i < 10:
+        if i < TARGETS:
             loss += 1 - float(score)
         else:
             loss += float(score) - 0
